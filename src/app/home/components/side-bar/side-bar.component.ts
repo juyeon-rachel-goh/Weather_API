6d679516b748068adea77812ef9eae5442640726
Rachel Goh <rachelg0129@gmail.com>
2022-06-14 18:47:51 -0400

    search function updated

--- src/app/home/components/side-bar/side-bar.component.ts
+++ src/app/home/components/side-bar/side-bar.component.ts
@@ -1,6 +1,7 @@
 import { TitleCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { NgbTypeaheadSelectItemEvent } from '@ng-bootstrap/ng-bootstrap';
 import {
   map,
   switchMap,
@@ -21,68 +22,6 @@ import { GeocodeService } from '../../services/geocode/geocode.service';
 import { LocalStorageService } from '../../services/local-storage/local-storage.service';
 import { WeatherStateService } from '../../services/weather-state/weather-state.service';
 import { WeatherService } from '../../services/weather/weather.service';
-
-const states = [
-  'Alabama',
-  'Alaska',
-  'American Samoa',
-  'Arizona',
-  'Arkansas',
-  'California',
-  'Colorado',
-  'Connecticut',
-  'Delaware',
-  'District Of Columbia',
-  'Federated States Of Micronesia',
-  'Florida',
-  'Georgia',
-  'Guam',
-  'Hawaii',
-  'Idaho',
-  'Illinois',
-  'Indiana',
-  'Iowa',
-  'Kansas',
-  'Kentucky',
-  'Louisiana',
-  'Maine',
-  'Marshall Islands',
-  'Maryland',
-  'Massachusetts',
-  'Michigan',
-  'Minnesota',
-  'Mississippi',
-  'Missouri',
-  'Montana',
-  'Nebraska',
-  'Nevada',
-  'New Hampshire',
-  'New Jersey',
-  'New Mexico',
-  'New York',
-  'North Carolina',
-  'North Dakota',
-  'Northern Mariana Islands',
-  'Ohio',
-  'Oklahoma',
-  'Oregon',
-  'Palau',
-  'Pennsylvania',
-  'Puerto Rico',
-  'Rhode Island',
-  'South Carolina',
-  'South Dakota',
-  'Tennessee',
-  'Texas',
-  'Utah',
-  'Vermont',
-  'Virgin Islands',
-  'Virginia',
-  'Washington',
-  'West Virginia',
-  'Wisconsin',
-  'Wyoming',
-];
 
 @Component({
   selector: 'app-side-bar',
@@ -110,7 +49,7 @@ export class SideBarComponent implements OnInit {
     text$: Observable<string>
   ) =>
     text$.pipe(
-      debounceTime(500),
+      debounceTime(100),
       distinctUntilChanged(),
       switchMap((search) =>
         this.geocodeService.getLatLong(search).pipe(
@@ -140,52 +79,28 @@ export class SideBarComponent implements OnInit {
     }
   };
 
+  public onSelect(event: NgbTypeaheadSelectItemEvent<Location>): void {
+    this.searchFormControl.setValue(event.item);
+    this.search();
+  }
+
   public search(isResearch: boolean = false): void {
-    if (!isResearch) {
-      this.localStorageService.saveSearch(this.searchFormControl.value);
-      this.localStorageService.next(this.localStorageService.retriveSearch());
-    }
-    this.geocodeService
-      .getLatLong(this.searchFormControl.value)
-      .pipe(
-        take(1),
-        tap((response) => {
-          if (response?.features && response.features.length === 0) {
-            throw new Error('Invalid API response.');
-          }
-        }),
-        map((response) => {
-          //function
-          if (response?.features && response.features.length > 0) {
-            const properties = response.features[0].properties;
-            const location: Location = {
-              lat: properties.lat,
-              lon: properties.lon,
-              city: properties.city,
-            };
-            this.geocodeStateService.next(location);
-            return location;
-          }
-          return {};
-        })
-      )
-      .subscribe({
-        next: (location) => {
-          this.weatherService
-            .getWeatherData(location)
-            .pipe(take(1))
-            .subscribe((response) => {
-              const weather: Weather = {
-                current: this.weatherStateService.format(response.current),
-                daily: (response.daily as any[]).map((data) =>
-                  this.weatherStateService.format(data)
-                ),
-              };
-              this.weatherStateService.next(weather);
-              console.log(weather);
-            });
-        },
-        error: (e) => console.error(e), // TODO: display an error. add something
+    // if (!isResearch) {
+    //   this.localStorageService.saveSearch(this.searchFormControl.value);
+    //   this.localStorageService.next(this.localStorageService.retriveSearch());
+    // }
+    this.weatherService
+      .getWeatherData(this.searchFormControl.value)
+      .pipe(take(1))
+      .subscribe((response) => {
+        const weather: Weather = {
+          current: this.weatherStateService.format(response.current),
+          daily: (response.daily as any[]).map((data) =>
+            this.weatherStateService.format(data)
+          ),
+        };
+        this.weatherStateService.next(weather);
+        console.log(weather);
       });
   }
 
