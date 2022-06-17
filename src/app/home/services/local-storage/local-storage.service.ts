961c5a2faa88f474b4532c99865ae48bb8fadca2
Rachel Goh <rachelg0129@gmail.com>
2022-06-17 18:31:18 -0400

    deleted comments

--- src/app/home/services/local-storage/local-storage.service.ts
+++ src/app/home/services/local-storage/local-storage.service.ts
@@ -2,23 +2,17 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Location } from '../../model/location.interface';
 
-// make sure to parse what we pull out of localstorage
-
 @Injectable({
   providedIn: 'root',
 })
 export class LocalStorageService {
   includes: any;
   constructor() {
-    //do stuff
-    // check if localstorge has data
     const cities = this.retriveSearch();
     if (Array.isArray(cities) && cities.length > 0) {
       this.next(cities);
       console.log(cities);
     }
-    // if it has a data, push .next function
-    // if nothing, do nothing
   }
 
   private mostRecentLocalStorageData: BehaviorSubject<Location[]> =
@@ -37,12 +31,8 @@ export class LocalStorageService {
       localStorage.setItem('search', JSON.stringify([location]));
     }
   }
-
-  // take out the data out of localstorage
   public retriveSearch(): Location[] {
-    // get into the local storage using key
     const searchString = localStorage.getItem('search');
-    // reformat the data string -> array
     if (searchString) {
       const savedCities = JSON.parse(searchString);
       return savedCities;
