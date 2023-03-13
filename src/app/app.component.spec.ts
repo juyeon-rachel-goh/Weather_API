0c4add67c0893852b54c6085f48d1d8f9310e359
Rachel Goh <rachelg0129@gmail.com>
2023-03-13 14:00:10 -0400

    deployment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project-2'`, () => {
+  it(`should have as title 'Weather-Forecast'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project-2');
+    expect(app.title).toEqual('Weather-Forecast');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('project-2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'Weather-Forecast app is running!'
+    );
   });
 });
